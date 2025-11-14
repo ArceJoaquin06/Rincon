cb3dc70f520313dfbb33da4407be3382a1824bb2
Joaquin Arce <joaquinarce@ubp.edu.ar>
2025-11-14 11:38:24 -0300

    creo los primeros archivos de main

---
+++ El_Rincon/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: "http://localhost:13847/" //Tiene que coincidir con el puerto en donde corro, si uso una api real
+     //Tiene que ser el local host del Swagger
+};
