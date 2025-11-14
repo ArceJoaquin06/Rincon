d71074ac8543afe73016bcc4befd52c388f53c68
ArceJoaquin <arcejoaquin06@gmail.com>
2025-11-14 18:30:59 -0300

    hice las primeras paginas con angular

--- El_Rincon/src/app/main/main-routing.module.ts
+++ El_Rincon/src/app/main/main-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+import { RegistrarseComponent } from './pages/registrarse/registrarse.component';
+import { InstitucionComponent } from './pages/institucion/institucion.component';
 
 const routes: Routes = [
-  { path: 'inicio', component: InicioComponent }
+  { path: 'inicio', component: InicioComponent },
+  { path: 'inicio-sesion', component: InicioSesionComponent},
+  { path: 'registrarse', component: RegistrarseComponent},
+  { path: 'institucion', component: InstitucionComponent}
 ];
 
 @NgModule({
--- El_Rincon/src/app/main/main.module.ts
+++ El_Rincon/src/app/main/main.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+import { RegistrarseComponent } from './pages/registrarse/registrarse.component';
+import { InstitucionComponent } from './pages/institucion/institucion.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    InicioComponent
+    InicioComponent,
+    InicioSesionComponent,
+    RegistrarseComponent,
+    InstitucionComponent
   ],
   imports: [
     CommonModule,
